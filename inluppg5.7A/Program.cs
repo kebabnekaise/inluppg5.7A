b208a78077b6edb8f5b9d6ab6db90511197f0707
kebabnekaise <emsi06001@utb.vaxjo.se>
2023-11-28 12:17:10 +0100

    Add project files.

---
+++ inluppg5.7A/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace inluppg5_7A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lander = new string[] { "1. Kina", "2. Indien", "3. USA", "4. Indonesien", "5. Pakistan" };    
+
+            foreach (string land in lander)
+            {
+                Console.WriteLine(land);
+            }
+
+            Console.WriteLine("Skriv in siffran för landet som ska skrivas ut");
+            string indecesInput = Console.ReadLine();
+
+            string[] indeces = indecesInput.Split(' ');
+
+            Console.WriteLine();
+            for (int i = 0;  i < indeces.Length; i++)
+            {
+                Console.WriteLine(lander[int.Parse(indeces[i]) - 1]);
+            }
+        }
+    }
+}
